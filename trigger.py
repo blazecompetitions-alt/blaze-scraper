79e03ffb5d26cfe7b09af9a6dff1e3307d7a0c90
blazecompetitions-alt <blazecompetitions@gmail.com>
2026-06-23 07:54:46 +0100

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -1,19 +1,19 @@
 from flask import Flask
-import blaze_scraper   # ✅ ADD THIS
+import blaze_scraper
 
 app = Flask(__name__)
-
-@app.route("/run")
-def run_script():
-    try:
-        blaze_scraper.main()   # 👈 call function directly
-        return "✅ Sheet updated successfully"
-    except Exception as e:
-        return f"❌ Error: {e}"
 
 @app.route("/")
 def home():
     return "Blaze scraper is running"
+
+@app.route("/run")
+def run_script():
+    try:
+        blaze_scraper.main()
+        return "✅ Sheet updated successfully"
+    except Exception as e:
+        return f"❌ Error: {e}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
